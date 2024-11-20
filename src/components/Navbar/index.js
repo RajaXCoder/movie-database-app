041dc4d59809513d0f6d7a3c7002ff69d2dc5184
RajaXCoder <rajasekarrdx35@gmail.com>
2024-11-20 18:13:04 +0000

    add mobile view navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,11 +4,16 @@ import './style.css'
 
 const Navbar = () => {
   const [query, setQuery] = useState('')
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false) // Track navbar toggle state
   const navigate = useNavigate()
 
   const handleSearch = e => {
     e.preventDefault()
     if (query) navigate(`/search?query=${query}`)
+  }
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(prevState => !prevState) // Toggle navbar open/close
   }
 
   return (
@@ -18,7 +23,15 @@ const Navbar = () => {
           <Link to="/">movieDB</Link>
         </h1>
       </div>
-      <div className="navbar-links">
+      {/* Change the div to a button and add aria-label for accessibility */}
+      <button
+        className="navbar-toggle"
+        onClick={toggleNavbar}
+        aria-label="Toggle navigation menu"
+      >
+        &#9776; {/* Hamburger icon */}
+      </button>
+      <div className={`navbar-links ${isNavbarOpen ? 'open' : ''}`}>
         <Link to="/">Popular</Link>
         <Link to="/top-rated">Top Rated</Link>
         <Link to="/upcoming">Upcoming</Link>
